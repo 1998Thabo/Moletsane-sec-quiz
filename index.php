11a1f63f07c1e4859fb4acfdb7c274816c870f48
Thabo Mohlamme <38581060+1998Thabo@users.noreply.github.com>
2019-06-12 09:10:53 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Test web application</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+
+
+<body>
+    <form class="box" action="validation.php" method="post">
+        <h1>Login</h1>
+        <input type="text" name="usr" placeholder="username">
+        <input type="password" name="pwd" placeholder="password">
+        <input type="submit" name="submit" value="Login">
+</form>
+
+</body>
+
+</html>
